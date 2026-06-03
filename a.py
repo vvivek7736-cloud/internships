34d374a5c550f98e42958a8f6d3178553d8f199c
Vivek V <vvivek7736@gmail.com>
2026-06-03 17:02:22 +0530

    added set and dictionaries

--- a.py
+++
@@ -1,6 +0,0 @@
-a=[10,65,78,15,65,5,2,8,10]
-count=0
-for i in a:
-    if(i%2!=0):
-        count+=1
-print("no: of odd : ",count)
---
+++ c.py
@@ -0,0 +1,14 @@
+marks={
+    'varun' : 10,
+    'arun' : 20,
+    'vivek' : 30,
+    'anoop' : 15
+
+}
+stud={
+    'name' : 'vivek',
+    'age' : 20,
+    'course' : 'aiml'
+}
+stud.update(marks)
+print(stud)
